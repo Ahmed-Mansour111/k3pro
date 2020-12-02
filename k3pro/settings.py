5796c495bd8f5e8f8fbc63baafc02af9948b9a0b
ahmed-mansour111 <ahmed.m.mansour1111@gmail.com>
2020-12-02 14:44:34 +0200

    staticfiles back

--- k3pro/settings.py
+++ k3pro/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),

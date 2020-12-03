355713e72173948b68e9d4647f0c379716f3d347
ahmed-mansour111 <ahmed.m.mansour1111@gmail.com>
2020-12-03 14:17:01 +0200

    email setting disable

--- k3pro/settings.py
+++ k3pro/settings.py
@@ -169,10 +169,10 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='ahmed.sayed12396@gmail.com'
-EMAIL_HOST_PASSWORD='@hm3d2100'
-EMAIL_USE_TLS=True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER='ahmed.sayed12396@gmail.com'
+# EMAIL_HOST_PASSWORD='@hm3d2100'
+# EMAIL_USE_TLS=True
 
 
